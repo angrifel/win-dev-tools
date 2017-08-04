f4a47bd1297d72e889107074a0a1508946d5c632
angrifel <angrifel@users.noreply.github.com>
2017-08-04 18:46:46 -0300

    added statement to get-sysinternals to connect to SMB share

--- get-sysinternals.cmd
+++ get-sysinternals.cmd
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 SET SOURCE_DIRECTORY=\\live.sysinternals.com\tools\
-set TARGET_DIRECTORY=%~dp0sysinternals\
+SET TARGET_DIRECTORY=%~dp0sysinternals\
+net use \\live.sysinternals.com\tools\
 xcopy "%SOURCE_DIRECTORY%" "%TARGET_DIRECTORY%" /S /Y
 endlocal
